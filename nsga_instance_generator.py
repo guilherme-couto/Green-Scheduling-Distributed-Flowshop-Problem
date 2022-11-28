88bdb4643b2f40a479c3fcb974a91047b606fb7c
Patrick448 <patrickcarvalho448@gmail.com>
2022-11-28 16:16:13 -0300

    Create nsga_instance_generator.py

---
+++ nsga_instance_generator.py
@@ -0,0 +1,71 @@
+import random
+
+nums_factories = [2, 3, 4, 5]
+nums_jobs = [20, 40, 60, 80, 100]
+nums_machines = [4, 8, 16]
+v = [1, 1.3, 1.55, 1.75, 2.10]
+
+# Estrutura do aquivo gerado:
+
+# Fábricas
+# Máquinas
+# Jobs
+# Velocidades
+# Matriz MaquinasxJobs:
+#       Cada linha i é uma máquina
+#       Cada coluna j é um job
+
+
+class Instance:
+    factories = 0
+    machines = 0
+    num_jobs = 0
+    time_matrix = []
+
+    def __init__(self, factories, machines, jobs):
+        self.factories = factories
+        self.machines = machines
+        self.num_jobs = jobs
+        self.time_matrix = [[None]*jobs for i in range(machines)]
+
+    def to_text(self):
+        instance_string = f"{self.factories}\n{self.machines}\n{self.num_jobs}\n"
+        speed_modes_str = " ".join(map(str, v))
+        instance_string += f"{speed_modes_str}\n"
+        for i in range(self.machines):
+            for j in range(self.num_jobs):
+                instance_string += f"{self.time_matrix[i][j]} "
+        return instance_string
+
+
+def generate(seed=0):
+    instance_list = []
+    random.seed(seed)
+    for f in nums_factories:
+        for m in nums_machines:
+            for n in nums_jobs:
+                instance = Instance(f, m, n)
+
+                for i in range(m):
+                    for j in range(n):
+                        instance.time_matrix[i][j] = int(random.uniform(5, 50))
+
+                instance_list.append(instance)
+
+    return instance_list
+
+
+def main():
+    instance_list = generate(seed=0)
+    generate_instance_files(path="instances", instance_list=instance_list)
+
+
+def generate_instance_files(path="", instance_list=[]):
+    for i, instance in enumerate(instance_list):
+        os = open(f"{path}/{instance.factories}-{instance.machines}-{instance.num_jobs}__{i}.txt", mode='x')
+        os.write(instance.to_text())
+        os.close()
+
+
+if __name__ == "__main__":
+    main()
