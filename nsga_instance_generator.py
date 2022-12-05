99d309e03fda29968d2f67513ffa82f0bce2ff1f
Patrick448 <patrickcarvalho448@gmail.com>
2022-12-05 15:58:56 -0300

    gera as 600 instancias e cria diretorio para cada seed usada

--- nsga_instance_generator.py
+++ nsga_instance_generator.py
@@ -1,4 +1,7 @@
+import datetime
 import random
+import os
+import time
 
 nums_factories = [2, 3, 4, 5]
 nums_jobs = [20, 40, 60, 80, 100]
@@ -41,30 +44,41 @@ class Instance:
 def generate(seed=0):
     instance_list = []
     random.seed(seed)
-    for f in nums_factories:
-        for m in nums_machines:
-            for n in nums_jobs:
-                instance = Instance(f, m, n)
+    for _ in range(10):
+        for f in nums_factories:
+            for m in nums_machines:
+                for n in nums_jobs:
+                    instance = Instance(f, m, n)
 
-                for i in range(m):
-                    for j in range(n):
-                        instance.time_matrix[i][j] = int(random.uniform(5, 50))
+                    for i in range(m):
+                        for j in range(n):
+                            instance.time_matrix[i][j] = int(random.uniform(5, 50))
 
-                instance_list.append(instance)
+                    instance_list.append(instance)
 
     return instance_list
 
 
 def main():
-    instance_list = generate(seed=0)
-    generate_instance_files(path="instances", instance_list=instance_list)
+    seed = int(time.time()*1000 % 1000)
+    instance_list = generate(seed=seed)
+    create_dir_if_not_exists("instances")
+    create_dir_if_not_exists(f"instances/{seed}")
+    generate_instance_files(path=f"instances/{seed}", instance_list=instance_list)
 
 
 def generate_instance_files(path="", instance_list=[]):
     for i, instance in enumerate(instance_list):
-        os = open(f"{path}/{instance.factories}-{instance.machines}-{instance.num_jobs}__{i}.txt", mode='x')
-        os.write(instance.to_text())
-        os.close()
+        file = open(f"{path}/{instance.factories}-{instance.machines}-{instance.num_jobs}__{i}.txt", mode='x')
+        file.write(instance.to_text())
+        file.close()
+
+
+def create_dir_if_not_exists(path):
+    try:
+        os.mkdir(path)
+    except FileExistsError:
+        pass
 
 
 if __name__ == "__main__":
