cbe426ce5e146c073d87d9c934911d30b4048165
GuilhermeC <guiguimcouto@gmail.com>
2022-12-05 11:39:41 -0300

    Mudanças na classe Matrices e add atributo em Sol

--- include/Matrices.hpp
+++ include/Matrices.hpp
@@ -3,4 +3,48 @@
 
 #include "defines.hpp"
 
+class Matrices
+{
+private:
+    int F;                              // Number of factories
+    int n;                              // Number of jobs
+    int m;                              // Number of machines
+    vector<vector<int>> t;              // Standard processing time matrix (line = machine, column = job)
+    vector<float> speeds;               // Available processing speeds
+
+public:
+    Matrices(int F, int n, int m, vector<vector<int>> t);      // Constructor (F = number of factories, n = number of jobs, m = number of machines)
+    ~Matrices();
+
+    int getF();
+    int get_n();
+    int get_m();
+    vector<vector<int>> getMatrix_t();
+    vector<float> getVectorSpeeds();
+
+    int get_t(int machine_id, int job_id);
+    int getSpeed(int i);
+};
+
+Matrices::Matrices(int F, int n, int m, vector<vector<int>> t)
+{
+    this->F = F;
+    this->n = n;
+    this->m = m;
+    this->t = t;
+
+    this->speeds.push_back(1.0);
+    this->speeds.push_back(1.3);
+    this->speeds.push_back(1.55);
+    this->speeds.push_back(1.75);
+    this->speeds.push_back(2.1);
+}
+
+Matrices::~Matrices()
+{
+}
+
+
+
+
 #endif // MATRICES_HPP
--- include/Solution.hpp
+++ include/Solution.hpp
@@ -12,20 +12,24 @@ private:
     float TFT;                          // Total flow time  
     vector<float> EC_f;                 // Energy consumption of each factory
     vector<float> FT_f;                 // Flow time of each factory    
+    vector<int> job_allocation;         // Factory id of each job
 
 public:
-    Solution(int n, int m, int F);    // Constructor (n = number of jobs, m = number of machines, F = number of factories)  
+    Solution(int n, int m, int F);      // Constructor (n = number of jobs, m = number of machines, F = number of factories)  
     ~Solution();
 
     vector<vector<int>> getDistribution();
     vector<vector<float>> getV();
     float getTEC();
     float getTFT();
+    vector<int> getAllJobsAllocation();
+    int getJobAllocation(int job_id);
 
     void setSequence(int f_id, vector<int> seq);
     void setV(int i, int j, float v);
     void setEC_f(int f_id, float ec);
     void setFT_f(int f_id, float ft);
+    void setJobAllocation(int job_id, int f_id);
 
 };
 
